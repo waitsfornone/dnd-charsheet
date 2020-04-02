2e0e0277d50916baa75fa83b465ac032a212345d
Tim Enders <enders.tim@gmail.com>
2020-04-02 11:52:27 -0500

    finished up the basic layout for the header row. Progress!"

--- src/app.jsx
+++ src/app.jsx
@@ -1,14 +1,5 @@
 import React from 'react';
 
-function rectangle(width, height, border) {
-  const styleObj = {
-    width: width,
-    height: height,
-    border: `${border}px solid #000`,
-  };
-  return styleObj;
-}
-
 function HeaderRow() {
   // this is the top row of data for the sheet
   return (
@@ -17,11 +8,29 @@ function HeaderRow() {
         <img className="profile-image" src="../pics/my_pic.jpg" alt="gen_face.png" height="100" width="100" />
         <p id="char-name">Lyque</p>
       </div>
-      <div className="group" id="char-info" style={rectangle(200, 100, 1)}>
-        blah
+      <div className="group" id="char-info">
+        <div className="group" id="pers-info">
+          <div className="group-col">
+            <p>Class</p>
+            <p>Race</p>
+          </div>
+          <div className="group-col">
+            <p>Background</p>
+            <p>Alignment</p>
+          </div>
+        </div>
+        {/* This will need to determine if the game is points based and remove the bottom number if it is milestones  */}
+        <div className="group-col">
+          <p>Experience Gained</p>
+          <p>Experience for next level</p>
+        </div>
+        <p>Level</p>
       </div>
       <div className="group" id="char-hp">
-        HP/HP
+        <div className="group-col">
+          <p>HP/HP</p>
+          <p>Temp HP</p>
+        </div>
       </div>
       <div className="group" id="char-inspiration">
         Inspiration
@@ -32,13 +41,13 @@ function HeaderRow() {
 
 class Sheet extends React.Component {
 
-  renderRow(i) {
-    return (
-      <Row
-        value={i}
-      />
-    );
-  }
+  // renderRow(i) {
+  //   return (
+  //     <Row
+  //       value={i}
+  //     />
+  //   );
+  // }
 
   render() {
     return (
